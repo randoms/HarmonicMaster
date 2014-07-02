a5236076f452ec38edf36e1b451aea1c4bb3be6e
randoms <newnewton@qq.com>
2014-07-02 18:02:18 +0800

    add actionbar

--- src/HarmonicMaster/app/src/main/java/me/randoms/harmonicmaster/TestActivity.java
+++ src/HarmonicMaster/app/src/main/java/me/randoms/harmonicmaster/TestActivity.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.Window;
 
 public class TestActivity extends Activity {
 	private ActionBar actionBar;
@@ -12,15 +14,21 @@ public class TestActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-        //getWindow().requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
         //getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         ActionBar actionBar = getActionBar();
 	    setContentView(R.layout.activity_main_v1);
         actionBar = getActionBar();
 
-        actionBar.setBackgroundDrawable(new ColorDrawable(Color.argb(155,255,255,255)));
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.argb(10,0,0,0)));
+        actionBar.setDisplayShowTitleEnabled(false);
 
 	}
-	
-	
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+
+    }
 }
